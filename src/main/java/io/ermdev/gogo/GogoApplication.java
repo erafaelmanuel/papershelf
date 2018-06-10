71b3797a805ef58ec9cb8c97f6bf427df6d7eb63
erafaelmanuel <erafaelmanuel@gmail.com>
2018-06-10 14:07:03 +0800

    Initial commit

---
+++ src/main/java/io/ermdev/gogo/GogoApplication.java
@@ -0,0 +1,12 @@
+package io.ermdev.gogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogoApplication.class, args);
+	}
+}
