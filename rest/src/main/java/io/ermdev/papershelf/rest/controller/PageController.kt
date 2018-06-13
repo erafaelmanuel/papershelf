6943db2d6a6fa90bc8c53e419111299a0e158db6
erafaelmanuel <erafaelmanuel@gmail.com>
2018-06-13 14:59:42 +0800

    Update

---
+++ rest/src/main/java/io/ermdev/papershelf/rest/controller/PageController.kt
@@ -0,0 +1,79 @@
+package io.ermdev.papershelf.rest.controller
+
+import io.ermdev.papershelf.data.entity.Page
+import io.ermdev.papershelf.data.service.PageService
+import io.ermdev.papershelf.exception.EntityException
+import io.ermdev.papershelf.rest.Message
+import io.ermdev.papershelf.rest.dto.PageDto
+import io.ermdev.papershelf.rest.hateoas.PageHateoas.Companion.getSelfLink
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.Resources
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/pages")
+class PageController(val pageService: PageService) {
+
+    @GetMapping(produces = ["application/json"])
+    fun getPages(): ResponseEntity<Any> {
+        val resources = ArrayList<PageDto>()
+        pageService.findAll().forEach({ page ->
+            val dto = PageDto(id = page.id, number = page.number, image = page.image)
+
+            dto.add(getSelfLink(page.id))
+            resources.add(dto)
+        })
+        return ResponseEntity(Resources(resources, linkTo(this::class.java).withSelfRel()), HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/{pageId}"], produces = ["application/json"])
+    fun getPageById(@PathVariable("pageId") pageId: String): ResponseEntity<Any> {
+        return try {
+            val page = pageService.findById(pageId)
+            val dto = PageDto(id = page.id, number = page.number, image = page.image)
+
+            dto.add(getSelfLink(page.id))
+            ResponseEntity(Resource(dto), HttpStatus.OK)
+        } catch (e: EntityException) {
+            val message = Message(status = 404, error = "Not Found", message = e.message)
+            ResponseEntity(message, HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PostMapping(consumes = ["application/json"])
+    fun addPage(@RequestBody body: Page): ResponseEntity<Any> {
+        return try {
+            pageService.save(body)
+            ResponseEntity(HttpStatus.CREATED)
+        } catch (e: EntityException) {
+            val message = Message(status = 400, error = "Bad Request", message = e.message)
+            ResponseEntity(message, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @PutMapping(value = ["/{pageId}"], consumes = ["application/json"])
+    fun updatePageById(@PathVariable("pageId") pageId: String,
+                       @RequestBody body: Page): ResponseEntity<Any> {
+        return try {
+            val page = pageService.findById(pageId)
+
+            page.number = body.number
+            page.image = body.image
+            pageService.save(page)
+            ResponseEntity(HttpStatus.OK)
+        } catch (e: EntityException) {
+            val message = Message(status = 400, error = "Bad Request", message = e.message)
+            ResponseEntity(message, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @DeleteMapping("/{pageId}")
+    fun deletePageById(@PathVariable("pageId") pageId: String): ResponseEntity<Any> {
+        pageService.deleteById(pageId)
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+}
