0ea3c0bb1c3c35e4407288d246981534202a698f
erafaelmanuel <erafaelmanuel@gmail.com>
2018-06-20 20:54:25 +0800

    Update

--- rest/src/main/java/io/ermdev/papershelf/rest/chapter/ChapterController.kt
+++ rest/src/main/java/io/ermdev/papershelf/rest/chapter/ChapterController.kt
@@ -4,6 +4,8 @@ import io.ermdev.papershelf.data.entity.Chapter
 import io.ermdev.papershelf.data.service.ChapterService
 import io.ermdev.papershelf.exception.EntityException
 import io.ermdev.papershelf.rest.Message
+import io.ermdev.papershelf.rest.page.PageController
+import io.ermdev.papershelf.rest.page.PageDto
 import org.springframework.hateoas.Resource
 import org.springframework.hateoas.Resources
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
@@ -36,6 +38,25 @@ class ChapterController(val chapterService: ChapterService) {
 
             dto.add(linkTo(methodOn(this::class.java).getChapterById(chapter.id)).withSelfRel())
             ResponseEntity(Resource(dto), HttpStatus.OK)
+        } catch (e: EntityException) {
+            val message = Message(status = 404, error = "Not Found", message = e.message)
+            ResponseEntity(message, HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @GetMapping("/{chapterId}/pages")
+    fun getPages(@PathVariable("chapterId") chapterId: String): ResponseEntity<Any> {
+        return try {
+            val resources = ArrayList<PageDto>()
+
+            chapterService.findById(chapterId).pages.forEach({ page ->
+                val dto = PageDto(id = page.id, number = page.number, image = page.image)
+
+                dto.add(linkTo(methodOn(PageController::class.java).getPageById(page.id)).withSelfRel())
+                resources.add(dto)
+            })
+            ResponseEntity(Resources(resources, linkTo(methodOn(this::class.java))
+                    .withRel("pages")), HttpStatus.OK)
         } catch (e: EntityException) {
             val message = Message(status = 404, error = "Not Found", message = e.message)
             ResponseEntity(message, HttpStatus.NOT_FOUND)
