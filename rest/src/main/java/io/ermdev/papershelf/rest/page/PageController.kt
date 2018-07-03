8bfe355974811522a7412547d466f8e207a1bc98
erafaelmanuel <erafaelmanuel@gmail.com>
2018-07-03 23:39:11 +0800

    Update

--- rest/src/main/java/io/ermdev/papershelf/rest/page/PageController.kt
+++ rest/src/main/java/io/ermdev/papershelf/rest/page/PageController.kt
@@ -6,30 +6,21 @@ import io.ermdev.papershelf.data.service.PageService
 import io.ermdev.papershelf.exception.PaperShelfException
 import io.ermdev.papershelf.exception.ResourceException
 import io.ermdev.papershelf.rest.Message
-import io.ermdev.papershelf.rest.ResourceFinder.Companion.getLocalFile
-import org.apache.commons.io.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Pageable
 import org.springframework.hateoas.Resource
 import org.springframework.hateoas.Resources
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.util.StringUtils
 import org.springframework.web.bind.annotation.*
-import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/pages")
 class PageController(@Autowired val pageService: PageService,
-                     @Autowired val chapterService: ChapterService,
-                     @Autowired val request: HttpServletRequest) {
-
-    @Value("\${papershelf.path}")
-    lateinit var path: String
+                     @Autowired val chapterService: ChapterService) {
 
     @GetMapping(produces = ["application/json"])
     fun getPages(pageable: Pageable): ResponseEntity<Any> {
@@ -43,33 +34,17 @@ class PageController(@Autowired val pageService: PageService,
         return ResponseEntity(Resources(resources, linkTo(this::class.java).withSelfRel()), HttpStatus.OK)
     }
 
-    @GetMapping(value = ["/{pageId}"], produces = ["image/jpeg", "application/json"])
+    @GetMapping(value = ["/{pageId}"], produces = ["application/json"])
     fun getPageById(@PathVariable("pageId") pageId: String): ResponseEntity<Any> {
-        val headers = HttpHeaders()
         return try {
             val page = pageService.findById(pageId)
-            if (request.getHeader("Accept") == "application/json") {
-                val dto = PageDto(id = page.id, order = page.order, image = page.image, chapterId = page.chapter.id)
+            val dto = PageDto(id = page.id, order = page.order, image = page.image, chapterId = page.chapter.id)
 
-                dto.add(linkTo(methodOn(this::class.java).getPageById(page.id)).withSelfRel())
-                ResponseEntity(Resource(dto), HttpStatus.OK)
-            } else {
-                return try {
-                    val input = getLocalFile(path + page.image)
-
-                    ResponseEntity(IOUtils.toByteArray(input), HttpStatus.OK)
-                } catch (e: PaperShelfException) {
-                    val message = Message(status = 500, error = "Internal Server Error", message = e.message)
-
-                    headers.add("Content-Type", "application/json")
-                    ResponseEntity(message, headers, HttpStatus.INTERNAL_SERVER_ERROR)
-                }
-            }
+            dto.add(linkTo(methodOn(this::class.java).getPageById(page.id)).withSelfRel())
+            ResponseEntity(Resource(dto), HttpStatus.OK)
         } catch (e: PaperShelfException) {
             val message = Message(status = 404, error = "Not Found", message = e.message)
-
-            headers.add("Content-Type", "application/json")
-            ResponseEntity(message, headers, HttpStatus.NOT_FOUND)
+            ResponseEntity(message, HttpStatus.NOT_FOUND)
         }
     }
 
